caef134efa5ac7f77949ae071cd949eb336a4790
Josef Svenningsson <josef.svenningsson@gmail.com>
2010-05-21 18:47:16 +0200

    Started to do some code generation. It works!
    Ignore-this: 92f67ee0c4ddb4f3713632e2876d25a7
    
    darcs-hash:20100521164716-f316c-78125888a26c1cb2ed7769f2b575407e537e3e0a

--- Generate.hs
+++ Generate.hs
@@ -1,3 +1,4 @@
+{-# Language QuasiQuotes #-}
 {- The idea with this module is that it should generate modules with different
    amount of unrolling of lists. It should take a list of numbers and
    spit out modules which contains these amounts of unrolling
@@ -8,13 +9,78 @@ module Generate where
 
 import Language.Haskell.Exts.Syntax
 
-genModule n = Module 
+import Language.Haskell.Exts.QQ
+
+import Language.Haskell.Exts.Pretty
+
+genModule n = Module
               noLoc -- No location, we're generating the code
               (ModuleName ("Data.List.Unrolled" ++ show n)) 
               [] -- No pragmas
               Nothing -- No warning text
-              (Just []) -- Export list
+              Nothing -- Export list, I export everything
               [] -- Import decls
-              [] -- decls
+               -- decls
+              [listDeclaration n
+              ,elemIndexTy,elemIndex
+              ,elemIndicesTy,elemIndices
+              ,findTy,find
+              ,findIndexTy,findIndex
+              ,listToMaybeTy,listToMaybeNil,listToMaybeCons, listToMaybeNCons n
+              ]
 
 noLoc = SrcLoc "Unknown" 0 0
+
+listDeclaration n = -- data List a = Nil | Cons a (List a) | NCons a a (List a)
+  DataDecl noLoc DataType [] 
+               (Ident "List")
+               [UnkindedVar (Ident "a")]
+               [QualConDecl noLoc [] [] (ConDecl (Ident "Nil") [])
+               ,QualConDecl noLoc [] [] (ConDecl (Ident "Cons") 
+                                         [UnBangedTy (TyVar (Ident "a"))
+                                         ,UnBangedTy (TyApp (TyCon (UnQual (Ident "List"))) (TyVar (Ident "a")))
+                                         ])
+               ,QualConDecl noLoc [] [] (ConDecl (Ident "NCons")
+                                         (map UnBangedTy $
+                                          replicate n (TyVar (Ident "a")) ++
+                                          [TyApp (TyCon (UnQual (Ident "List"))) (TyVar (Ident "a"))]))
+               ]
+               [(UnQual (Ident "Show"),[])
+               ,(UnQual (Ident "Eq"),[])
+               ,(UnQual (Ident "Ord"),[])
+               ]
+
+{- This gives a very weird error message.
+listDeclaration n = [$dec| data List a = Nil | Cons a (List a) |]
+  where ncons = QualConDecl noLoc [] []
+                (ConDecl (Ident "NCons") ((replicate n (UnBangedTy (TyVar (Ident "a"))))
+                                          ++ [UnBangedTy (TyApp (TyCon (UnQual (Ident "List"))) (TyVar (Ident "a")))
+                                             ]))
+-}
+
+-- A couple of definitions that don't depend on the representation of lists
+elemIndexTy = [$dec| elemIndex       :: Eq a => a -> List a -> Maybe Int |]
+elemIndex   = [$dec| elemIndex x     = findIndex (x==)|]
+
+elemIndicesTy = [$dec| elemIndices     :: Eq a => a -> List a -> List Int |]
+elemIndices   = [$dec| elemIndices x   = findIndices (x==)                |]
+
+findTy = [$dec| find            :: (a -> Bool) -> List a -> Maybe a |]
+find   = [$dec| find p          = listToMaybe . filter p            |]
+
+
+findIndexTy = [$dec| findIndex       :: (a -> Bool) -> List a -> Maybe Int |]
+findIndex   = [$dec| findIndex p     = listToMaybe . findIndices p         |]
+
+-- Functions depending on the representation of lists
+listToMaybeTy   = [$dec| listToMaybe :: List a -> Maybe a    |]
+listToMaybeNil  = [$dec| listToMaybe Nil           = Nothing |]
+listToMaybeCons = [$dec| listToMaybe (Cons x _)    = Just x  |]
+listToMaybeNCons n = 
+    FunBind 
+    [Match noLoc (Ident "listToMaybe") 
+     [PParen (PApp (UnQual (Ident "NCons")) 
+                       (PVar (Ident "x") : replicate (n-1) PWildCard))] 
+      Nothing 
+      (UnGuardedRhs 
+       (App (Con (UnQual (Ident "Just"))) (Var (UnQual (Ident "x"))))) (BDecls [])]
