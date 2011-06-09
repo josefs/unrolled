d4d0f6bdff73fffb019f888b98b100da8cc448a4
Josef Svenningsson <josef.svenningsson@gmail.com>
2011-06-09 17:25:12 +0200

    Some changes to the benchmarking
    Ignore-this: 29954fc2ea3884ca2d2f8a22bec5c216
    
    darcs-hash:20110609152512-f316c-8ba798e1bb036ecaa3958eaad6d8b9a765e65e89

--- Time.hs
+++ Time.hs
@@ -1,25 +1,45 @@
 import Prelude
 import qualified Prelude as P
 import qualified Data.List.Unrolled as U
+import qualified Data.List.Unrolled3 as U3
+import qualified Data.List.Unrolled4 as U4
 
 import Criterion
 import Criterion.Main
+import Criterion.Config
+import Criterion.MultiMap
+import Data.Monoid
 
-main = defaultMain [
-        bench "standard sum/enum"  
-                  $ \n -> P.sum (P.enumFromTo (0+n::Int) (10000+n))
-       ,bench "unrolled sum/enum"  
-                  $ \n -> U.sum (U.enumFromToInt (0+n::Int) (10000+n))
-       ,bench "unrolled sum/enum alternative" 
-                  $ \n -> U.sum (U.enumFromToInt2 (0+n::Int) (10000+n))
+conf = defaultConfig {
+--         cfgPlot = singleton KernelDensity (SVG 800 600)
+--       , cfgPlotSameAxis = Last (Just True)
+       }
 
-
+main = defaultMainWith conf (return ()) [
+        bgroup "sum-enum" [
+         bench "standard"  
+                  $ whnf (\n -> P.sum (P.enumFromTo (0+n::Int) (10000+n))) 10
+        ,bench "unrolled1"
+                  $ whnf (\n -> U.sum (U.enumFromToInt (0+n::Int) (10000+n))) 10
+        ,bench "unrolled2"
+                  $ whnf (\n -> U.sum (U.enumFromToInt2 (0+n::Int) (10000+n))) 10
+        ,bench "unrolled generated 3"
+                  $ whnf (\n -> U3.sum (U3.enumFromToL (0+n::Int) (10000+n))) 10
+        ,bench "unrolled generated 4"
+                  $ whnf (\n -> U4.sum (U4.enumFromToL (0+n::Int) (10000+n))) 10
+        ]
+       ,bgroup "sum-filter-enum" [
        -- These benchmarks are a bit flawed since the standard version
        -- benefits from fusion
-       ,bench "standard sum/filter/enum"
-                  $ \n -> P.sum (P.filter odd (P.enumFromTo (0+n::Int) (10000+n)))
-       ,bench "unrolled sum/filter/enum"
-                  $ \n -> U.sum (U.filter odd (U.enumFromTo (0+n::Int) (10000+n)))
-       ,bench "standard sum/filter/enum realigning"
-                  $ \n -> U.sum (U.filterR odd (U.enumFromTo (0+n::Int) (10000+n)))
+         bench "standard"
+                   $ whnf (\n -> P.sum (P.filter odd (P.enumFromTo (0+n::Int) (10000+n)))) 10
+        ,bench "unrolled"
+                   $ whnf (\n -> U.sum (U.filter odd (U.enumFromTo (0+n::Int) (10000+n)))) 10
+        ,bench "unrolled reconstruct"
+                   $ whnf (\n -> U.sum (U.filterR odd (U.enumFromTo (0+n::Int) (10000+n)))) 10
+        ,bench "unrolled generated 3"
+                   $ whnf (\n -> U3.sum (U3.filter odd (U3.enumFromToL (0+n::Int) (10000+n)))) 10
+        ,bench "unrolled bad generated 3"
+                   $ whnf (\n -> U3.sumBad (U3.filter odd (U3.enumFromToL (0+n::Int) (10000+n)))) 10
+        ]
        ]
